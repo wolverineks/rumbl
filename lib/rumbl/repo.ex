7eeda7559a4cf5fe607ddd82e5b1509fb000c92b
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-06-05 20:15:14 -0700

    Add new/create user routes, templates

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,43 +1,4 @@
 defmodule Rumbl.Repo do
-
-  @moduledoc """
   use Ecto.Repo, otp_app: :rumbl
-  In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "Jose",  username: "josevalim",  password: "elixir"},
-     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids",  password: "7langs"},
-     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "4", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "5", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "6", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "7", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "8", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "9", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "10", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "11", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "12", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "13", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "14", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "15", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "16", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "17", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "18", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "19", name: "Chris", username: "chrismccord", password: "phx"},
-     %Rumbl.User{id: "20", name: "Chris", username: "chrismccord", password: "phx"}]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
 
 end
