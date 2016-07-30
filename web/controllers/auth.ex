dfe0077a5f3c74093f9bf251442a60fe05ce6907
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-07-29 20:50:21 -0700

    Add basic socket authentication

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,9 +13,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -34,7 +34,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -56,6 +56,14 @@ defmodule Rumbl.Auth do
         dummy_checkpw()
         {:error, :not_found, conn}
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
 end
